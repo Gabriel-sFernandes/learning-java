2a07c5464dbffc09348ee903607481bd3c6415af
Gabriel Santana Fernandes <bifernandes10@gmail.com>
2022-12-27 23:47:20 -0300

    Create exercicioQuadrante.java

---
+++ testJava/src/exercicioQuadrante.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class exercicioQuadrante {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
