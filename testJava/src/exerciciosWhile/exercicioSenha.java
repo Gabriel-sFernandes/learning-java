b07aa71c0e46adefd2f306ccd785c0f295a1a864
Gabriel Santana Fernandes <bifernandes10@gmail.com>
2022-12-28 22:58:05 -0300

    Create exercicioSenha.java

---
+++ testJava/src/exerciciosWhile/exercicioSenha.java
@@ -0,0 +1,23 @@
+package exerciciosWhile;
+import java.util.Scanner;
+
+public class exercicioSenha {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int senha = sc.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("Senha Invalida");
+            senha = sc.nextInt();
+        }
+        System.out.println("Acesso Permitido");
+        
+        sc.close();
+    }
+}
+
+
+
+
